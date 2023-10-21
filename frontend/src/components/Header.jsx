ece1b892c309601f686ad0bfb9617c6289381da1
Kevin Lee <kevinlfcola@gmail.com>
2023-10-21 01:14:53 -0700

    Add logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="sm" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">JustShop</Navbar.Brand>
+                    <Navbar.Brand href="/">
+                        <img
+                            src={logo}
+                            alt="logo"
+                            style={{
+                                width: "40px",
+                                height: "40px",
+                                margin: "10px",
+                            }}
+                        />
+                        JustShop
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
