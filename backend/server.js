621600bf4e24ded5bb47927bae0320adb8f3603e
GerardoAz <gerardoacedoz97@gmail.com>
2023-10-31 23:45:44 -0700

    Routes for products created Route for id of the product created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
-console.log("Hello World 123");
+import express from "express";
+import products from "./data/products.json" assert { type: "json" };
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const productId = req.params.id; // Get the product ID from the request parameters
+  let foundProduct = null;
+
+  products.products.forEach((p) => {
+    if (p._id === productId) {
+      foundProduct = p;
+    }
+  });
+
+  if (foundProduct) {
+    res.json(foundProduct);
+  } else {
+    res.status(404).json({ message: "Product not found" });
+  }
+});
+app.listen(port, () => console.log(`Server running on port ${port}`));
