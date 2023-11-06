e100a1b4d21d5e7ac6b14a93e7bfa05d1a9353fb
GerardoAz <gerardoacedoz97@gmail.com>
2023-11-06 15:35:49 -0800

    Fetched the products for Homescreen from the backend data folder

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import products from "./data/products.json" assert { type: "json" };
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import productsDummyData from "../data/products.json";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const HomeScreen = () => {
-    return (
-        <>
-            <h1>Latest Products</h1>
-            <Row>
-                {productsDummyData.products.map((product) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
-        </>
-    );
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await axios.get("/api/products");
+        console.log("data", data);
+        setProducts(data.products);
+      } catch (error) {
+        console.log("Error fetching products", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default HomeScreen;
