87d601fe8927499ab2b5b9642ce38716fec10c26
Kevin Lee <kevinlfcola@gmail.com>
2023-11-14 14:41:07 -0800

    Separate app.js and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,10 @@
-import express from "express";
-import dotenv from "dotenv";
+import app from "./app.js";
 import colors from "colors";
-import products from "./data/products.js";
-import cors from "cors";
 import connectDB from "./config/db.js";
-
-dotenv.config();
-
-const port = process.env.PORT || 5001;
 
 connectDB(); // Connect to MongoDB
 
-const app = express();
-
-app.use(cors());
-
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
-
-app.get("/api/products", (req, res) => {
-    res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-    const productId = req.params.id; // Get the product ID from the request parameters
-
-    let foundProduct = null;
-
-    products.forEach((p) => {
-        if (p._id === productId) {
-            foundProduct = p;
-        }
-    });
-
-    if (foundProduct) {
-        res.json(foundProduct);
-    } else {
-        res.status(404).json({ message: "Product not found" });
-    }
-});
+const port = process.env.PORT || 5001;
 
 app.listen(port, () =>
     console.log(`Server running on port ${port}!`.green.inverse)
