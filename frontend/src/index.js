c13d5cec9d04b8d04be0aee8fa3ba91992f97498
GerardoAz <gerardoacedoz97@gmail.com>
2023-11-29 12:21:59 -0800

    Shipping Page Created Payment Route Created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,10 @@ import "./assets/styles/custom.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-    RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
 } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
@@ -17,25 +17,27 @@ import HomePage from "./pages/HomePage.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
 import CartPage from "./pages/CartPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import ShippingPage from "./pages/ShippingPage.jsx";
 
 const routes = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<App />}>
-            <Route index={true} path="/" element={<HomePage />} />
-            <Route path="/product/:id" element={<ProductPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/login" element={<LoginPage />} />
-        </Route>
-    )
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomePage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/shipping" element={<ShippingPage />} />
+    </Route>
+  )
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={routes} />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
+  </React.StrictMode>
 );
 
 reportWebVitals();
