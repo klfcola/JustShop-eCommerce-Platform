b69ff634ae1596881b863f165fbb942be45253b4
Kevin Lee <kevinlfcola@gmail.com>
2023-12-12 13:15:11 -0800

    Pagination

--- frontend/src/pages/admin/ProductListPage.jsx
+++ frontend/src/pages/admin/ProductListPage.jsx
@@ -10,9 +10,15 @@ import {
     useDeleteProductMutation,
 } from "../../redux/slices/productsApiSlice.js";
 import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
+import Paginate from "../../components/Paginate.jsx";
 
 const ProductListPage = () => {
-    const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+    const { pageNumber } = useParams();
+
+    const { data, isLoading, error, refetch } = useGetProductsQuery({
+        pageNumber,
+    });
 
     const [createProduct, { isLoading: loadingCreate }] =
         useCreateProductMutation();
@@ -81,7 +87,7 @@ const ProductListPage = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {products.map((product) => (
+                            {data.products.map((product) => (
                                 <tr key={product._id}>
                                     <td>{product._id}</td>
                                     <td>{product.name}</td>
@@ -113,6 +119,11 @@ const ProductListPage = () => {
                             ))}
                         </tbody>
                     </Table>
+                    <Paginate
+                        pages={data.pages}
+                        page={data.page}
+                        isAdmin={true}
+                    />
                 </>
             )}
         </>
