5b230f6bea28681568060336ce1c9030a7e822b5
Kevin Lee <kevinlfcola@gmail.com>
2023-12-13 02:34:59 -0800

    Carousel

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -136,6 +136,14 @@ const createProductReview = asyncHandler(async (req, res) => {
     }
 });
 
+// Get top rated products
+// GET /api/products/top
+// Public
+const getTopProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+    res.status(200).json(products);
+});
+
 export {
     getProducts,
     getProductById,
@@ -143,4 +151,5 @@ export {
     updateProduct,
     deleteProduct,
     createProductReview,
+    getTopProducts,
 };
