953c9e110f37c90a7022066a8efa0c6ea6d60b7d
Kevin Lee <kevinlfcola@gmail.com>
2024-08-31 15:21:43 -0700

    Prepare the deployment with Render

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,6 +1,8 @@
-const port = process.env.PORT || 5001;
+// const port = process.env.PORT || 5001;
 
-export const BASE_URL = `http://localhost:${port}`;
+export const BASE_URL =
+  // `http://localhost:${port}`
+  process.env.REACT_APP_API_URL;
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
